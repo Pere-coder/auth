eeab4d2829b7695899b544a60743d18ccd72a768
pere-coder <godspowerpere@yahoo.com>
2023-11-17 19:43:04 +0100

    edits

--- components/login-btn.jsx
+++ components/login-btn.jsx
@@ -2,18 +2,23 @@ import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Component() {
   const { data: session } = useSession()
-  if (session) {
+
     return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
+      
+      <div className="bg-gray-200 w-full flex justify-evenly py-5">
+        {session ? (
+          <>
+          <h1 className="">Signed in as <span className="font-bold cursor-pointer">{session.user.email}</span></h1>
+          <button onClick={() => signOut()}>Sign out</button>
+         </>
+        ): (
+          <>
+           <h1> Not signed in</h1> 
+          <button onClick={() => signIn()}>Sign in</button>
+          </>
+         
+        )}
+        </div>
+        
     )
   }
-  return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
-  )
-}
